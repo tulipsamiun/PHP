58dcc43d9f05c4d0cf03648153b22bf1d0f9ae0b
tulipsamiun <tsamiun_ts@yahoo.com>
2021-06-01 02:56:44 +0600

    Update phpDestructor.php

--- general/phpDestructor.php
+++ general/phpDestructor.php
@@ -1,21 +1,22 @@
-<!DOCTYPE html>
 <html>
 <body>
 
 <?php
-class Fruit {
-  public $name;
-  public $color;
-
-  function __construct($name) {
-    $this->name = $name; 
-  }
-  function __destruct() {
-    echo "The fruit is {$this->name}."; 
-  }
+class Fruits {
+	  public $name;
+    public $color;
+    public $flv;
+    
+    function __construct($name, $color,  $flv){
+    	  $this->name = $name;
+        $this->color = $color;
+        $this->flv = $flv;
+    }
+    function __destruct(){
+    	echo "Fruit name: {$this->name} Color: {$this->color} Flavour: {$this->flv}";
+    }
 }
-
-$apple = new Fruit("Apple");
+$orange = new Fruits("Orange", "Red", "Fine");
 ?>
  
 </body>
